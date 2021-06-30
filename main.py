d990911cc2f4820241e17a9de217539fe82c68cf
Bertrand Bougon <bertrandbougon@gmail.com>
2021-06-30 18:24:15 +0200

    - Add openapi + docs (swagger) + redoc - start uvicorn on port 8081

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from fastapi import FastAPI
 from api import api_router
 
 app = FastAPI(
-    title="CRM Pilates"
+    title="CRM Pilates",
+    openapi_url="/openapi.json",
+    version="1",
+    docs_url="/docs",
+    redoc_url="/redoc",
 )
 
 app.include_router(api_router)
