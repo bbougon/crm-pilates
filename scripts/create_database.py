ae8493d1ba287a632ef52972e0bea295b2d84bad
Bertrand Bougon <bertrandbougon@gmail.com>
2021-09-22 22:03:43 +0200

    Add create database command into CI

--- scripts/create_database.py
+++ scripts/create_database.py
@@ -1,8 +1,8 @@
 import sqlite3
 
 
-def create_tables():
-    connect = sqlite3.connect("./local.persistent/event/event_store.db")
+def create_tables(db_file="./local.persistent/event/event_store.db"):
+    connect = sqlite3.connect(db_file)
     cursor = connect.cursor()
     cursor.execute(
         '''CREATE TABLE IF NOT EXISTS event (id text, root_id text, type text, timestamp text, payload text)''')
