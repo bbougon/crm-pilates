4d8ef320d19be82a6d3ee66302a1cec633701ba7
Bertrand Bougon <bertrand.bougon@gmail.com>
2021-10-24 19:03:55 +0200

    Refacto DateTimeComparator

--- domain/classroom/date_time_comparator.py
+++ domain/classroom/date_time_comparator.py
@@ -1,15 +1,15 @@
 from __future__ import annotations
 
 from abc import abstractmethod
-from datetime import datetime
+from datetime import datetime, date, time
 
 
 class Comparator:
 
-    def __init__(self, obj, object_to_compare) -> None:
+    def __init__(self, obj_to_compare, obj) -> None:
         super().__init__()
-        self._obj = obj
-        self._obj_to_compare = object_to_compare
+        self._object_to_compare = obj_to_compare
+        self._object = obj
 
     @abstractmethod
     def compare(self) -> bool:
@@ -18,51 +18,51 @@ class Comparator:
 
 class SameDayComparator(Comparator):
 
-    def __init__(self, date: datetime, date_to_compare: datetime) -> None:
-        super().__init__(date, date_to_compare)
+    def __init__(self, date_: date, date_to_compare: date) -> None:
+        super().__init__(date_, date_to_compare)
 
     def compare(self) -> bool:
-        return self._obj.date().weekday() == self._obj_to_compare.date().weekday()
+        return self._object_to_compare.weekday() == self._object.weekday()
 
 
 class SameTimeComparator(Comparator):
 
-    def __init__(self, obj, object_to_compare) -> None:
-        super().__init__(obj, object_to_compare)
+    def __init__(self, date_to_compare: time, date_: time) -> None:
+        super().__init__(date_to_compare, date_)
 
     def compare(self) -> bool:
-        return self._obj.time() == self._obj_to_compare.time()
+        return self._object_to_compare == self._object
 
 
 class SameDateComparator(Comparator):
 
-    def __init__(self, obj, object_to_compare) -> None:
-        super().__init__(obj, object_to_compare)
+    def __init__(self, date_to_compare: date, date_: date) -> None:
+        super().__init__(date_to_compare, date_)
 
     def compare(self) -> bool:
-        return self._obj.date() == self._obj_to_compare.date()
+        return self._object_to_compare == self._object
 
 
 class DateTimeComparator(Comparator):
 
-    def __init__(self, date: datetime, date_to_compare: datetime) -> None:
-        super().__init__(date, date_to_compare)
+    def __init__(self, date_to_compare: datetime, date_: datetime) -> None:
+        super().__init__(date_to_compare, date_)
         self.comparisons = []
 
     def same_day(self) -> DateTimeComparator:
-        self.comparisons.append(SameDayComparator(self._obj, self._obj_to_compare).compare())
+        self.comparisons.append(SameDayComparator(self._object_to_compare.date(), self._object.date()).compare())
         return self
 
     def same_time(self) -> DateTimeComparator:
-        self.comparisons.append(SameTimeComparator(self._obj, self._obj_to_compare).compare())
+        self.comparisons.append(SameTimeComparator(self._object_to_compare.time(), self._object.time()).compare())
         return self
 
     def before(self) -> DateTimeComparator:
-        self.comparisons.append(not self._obj_to_compare < self._obj)
+        self.comparisons.append(self._object_to_compare <= self._object)
         return self
 
     def same_date(self) -> DateTimeComparator:
-        self.comparisons.append(SameDateComparator(self._obj, self._obj_to_compare).compare())
+        self.comparisons.append(SameDateComparator(self._object_to_compare.date(), self._object.date()).compare())
         return self
 
     def compare(self) -> bool:
